3137ec7baf177baa61d649f71d43f2163d90e279
toastmanager <mrbonkyou@gmail.com>
2023-11-15 13:39:44 +0900

    removed unnecessary import

--- lib/pages/task_details.dart
+++ lib/pages/task_details.dart
@@ -1,4 +1,3 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:lifebeat/components/progressCircle.dart';
 import 'package:lifebeat/scripts/vars.dart';
