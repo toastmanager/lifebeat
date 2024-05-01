bd3660e712a9709945d179f4fabb4005e2a8ad8e
toastmanager <mrbonkyou@gmail.com>
2024-05-01 20:08:31 +0900

    daytime choose in new task page

--- lib/screens/new_task_page.dart
+++ lib/screens/new_task_page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:lifebeat/entities/task.dart';
 import 'package:lifebeat/main.dart';
+import '../components/surface.dart';
 
 class NewTaskPage extends StatefulWidget {
   const NewTaskPage({
@@ -17,6 +18,12 @@ class NewTaskPage extends StatefulWidget {
 class _NewTaskPageState extends State<NewTaskPage> {
   final nameController = TextEditingController();
   late final date = widget.date;
+  String dayTime = DayTime.morning;
+
+  @override
+  void initState() {
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -28,10 +35,34 @@ class _NewTaskPageState extends State<NewTaskPage> {
           children: [
             Column(
               children: [
-                TextField(
-                  controller: nameController,
-                  decoration: const InputDecoration(
-                    label: Text('Задача')
+                Surface(
+                  child: Column(
+                    children: [
+                      TextField(
+                        controller: nameController,
+                        decoration: const InputDecoration(
+                          label: Text('Задача')
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                const SizedBox(height: 20),
+                Surface(
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      const Text('Время суток'),
+                      DropdownButton(
+                        value: dayTime,
+                        items: const [
+                          DropdownMenuItem<String>(value: DayTime.morning, child: Text('Утро')),
+                          DropdownMenuItem<String>(value: DayTime.afternoon, child: Text('День')),
+                          DropdownMenuItem<String>(value: DayTime.evening, child: Text('Вечер')),
+                        ],
+                        onChanged: (value) => setState(() => dayTime = value!),
+                      )
+                    ],
                   ),
                 ),
               ],
@@ -50,7 +81,7 @@ class _NewTaskPageState extends State<NewTaskPage> {
                       objectbox.addTask(
                         nameController.text,
                         date,
-                        DayTime.afternoon,
+                        dayTime,
                       );
                       Navigator.of(context).pop();
                     },
